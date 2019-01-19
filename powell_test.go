4f6bf724c7518382e2127712e2dbe5d2db325e52
pa-m <pascal.masschelier@gmail.com>
2019-01-19 20:02:42 +0100

    minor change of ExamplePowellMinimizer

--- powell_test.go
+++ powell_test.go
@@ -15,13 +15,12 @@ func ExamplePowellMinimizer() {
 	pm.Logger = log.New(os.Stdout, "", 0)
 
 	pm.Minimize(
-		func(x []float64) float64 { return 1 - math.Exp(1/(1+x[0]*x[0]+x[1]*x[1]))/math.E },
+		func(x []float64) float64 { return -math.Exp(1 / (1 + x[0]*x[0] + x[1]*x[1])) },
 		[]float64{10, 20},
 	)
 	// Output:
 	// [-0.02748 -0.02037]
 	// [0.00818 -0.00407]
 	// [0.00154 -0.00337]
-	// [-0.00033 -0.00317]
-	// Success. Current function value: 1.016553e-05 Iterations: 4 Function evaluations: 113
+	// Success. Current function value: -2.718245 Iterations: 3 Function evaluations: 69
 }
